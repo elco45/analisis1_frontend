ef9f4983347caa9ad1c0cb82e00f72f49dacd320
baquedano <wolhaiksong.baquedano@gmail.com>
2016-08-29 15:09:29 -0400

    pseudo-drag-drop

--- app/scripts/controllers/RoomController.js
+++ app/scripts/controllers/RoomController.js
@@ -1,8 +1,8 @@
 angular.module('AngularScaffold.Controllers')
   .controller('RoomController', ['RoomService' ,  '$scope', '$state', '$stateParams','$rootScope', '$sessionStorage',
-  	function (RoomService, $scope, $state, $stateParams,$rootScope, $sessionStorage) {
-  	$scope.$sessionStorage = $sessionStorage;
-  	$scope.selectedRooms = [];
+    function (RoomService, $scope, $state, $stateParams,$rootScope, $sessionStorage) {
+    $scope.$sessionStorage = $sessionStorage;
+    $scope.selectedRooms = [];
     $scope.empleados = [];
     $scope.working_employee = [];
     $scope.firstfloor = [];
@@ -12,6 +12,11 @@ angular.module('AngularScaffold.Controllers')
     $scope.display_distribution = [];
     $scope.dragged_Employee ={};
     $scope.menuWasOpened = false;
+    $scope.isDragged = false;
+    $scope.Room_hovered = 0;
+    $scope.models = {
+      selected:null,
+      lists: {"empleados": [], "firstfloor": [], "secondfloor":[]} }
     //--------
     $scope.menuOptions = [
         ['Reservar por 1 dia', function (object) {
@@ -49,58 +54,110 @@ angular.module('AngularScaffold.Controllers')
             
         }]
     ];
-    //--
+    //--lo que hizo elena ---
+
+    $scope.init = function() {
+      $scope.getRooms();
+      $scope.llenarEmpleado();
+      console.log($scope.empleados)
+      $scope.models.lists.firstfloor = $scope.firstfloor;
+      $scope.models.lists.secondfloor = $scope.secondfloor
+      
+
+    }
+
+
+
+//---------------------------------------
+  
+          
+     
+
+
+
+
+
+
+
+    //------------------------- 
 
     if($state.params.content){
       $scope.roomSelected = $state.params.content.selectedRoomsv1;
     }
-
+    $scope.room_hover = function(event, ui, room){
+        $scope.Room_hovered = room.room_id;
+        console.log(room.room_id)
+    }
 
     $scope.startCallback = function(event, ui, employee) {
       //console.log('You started draggin: ' + employee.name);
       $scope.dragged_Employee = employee;
-    };
-    $scope.stopCallback = function(event, ui) {
-      console.log('--------------------------');
-    };
-    $scope.dropCallback = function(event, ui, type) {
-      console.log(type)
-      var index;
-
       console.log($scope.dragged_Employee)
-      if(type == 0){
-        for (var i = 0; i < $scope.empleados.length; i++) {
-          if($scope.empleados[i].username == $scope.dragged_Employee.username){
-            index = i
-            break;
-          }
-        }
-        $scope.empleados.splice(index,1);
-        $scope.working_employee.push($scope.dragged_Employee);
+      $scope.isDragged = true;
+    };
+    $scope.stopCallback = function(event, ui, employee) {
+      /*if($scope.Room_hovered < 200){
+        $scope.firstfloor[$scope.Room_hovered - 101].idUser.push(employee.username)
+      }else{
 
-      }/*else {
-        for (var i = 0; i < $scope.working_employee.length; i++) {
-          if($scope.working_employee[i].username == $scope.dragged_Employee.username){
-            index = i
-            break;
-          }
-        }
-        $scope.working_employee.splice(index,1)
-        $scope.empleados.push($scope.dragged_Employee)
-      }
-    */
-      console.log($scope.empleados)
-      console.log($scope.working_employee)
+        $scope.firstfloor[$scope.Room_hovered - 201].idUser.push(employee.username)
+      }*/
+      console.log($scope.firstfloor)
+      console.log($scope.dragged_Employee)
+    };
+    $scope.dropCallback = function(event, ui,room) {
+
+       $scope.sortRooms();
+      console.log(room)
+      console.log($scope.dragged_Employee)
+     
+      $scope.working_employee.push($scope.dragged_Employee);
 
       
     };
 
+    $scope.sortRooms= function(){
+      var j;
+     var flag = true;   // set flag to true to begin first pass
+     var temp;   //holding variable
+
+     while ( flag )
+     {
+            flag= false;    //set flag to false awaiting a possible swap
+            for( j=0;  j < $scope.firstfloor.length -1;  j++ )
+            {
+                   if ( $scope.firstfloor[ j ].room_id > $scope.firstfloor[j+1].room_id )   // change to > for ascending sort
+                   {
+                           temp = $scope.firstfloor[ j ];                //swap elements
+                           $scope.firstfloor[ j ] = $scope.firstfloor[ j+1 ];
+                           $scope.firstfloor[ j+1 ] = temp;
+                          flag = true;              //shows a swap occurred  
+                  } 
+            } 
+      } 
+      flag = true;
+       while ( flag )
+     {
+            flag= false;    //set flag to false awaiting a possible swap
+            for( j=0;  j < $scope.secondfloor.length -1;  j++ )
+            {
+                   if ( $scope.secondfloor[ j ].room_id > $scope.secondfloor[j+1].room_id )   // change to > for ascending sort
+                   {
+                           temp = $scope.secondfloor[ j ];                //swap elements
+                           $scope.secondfloor[ j ] = $scope.secondfloor[ j+1 ];
+                           $scope.secondfloor[ j+1 ] = temp;
+                          flag = true;              //shows a swap occurred  
+                  } 
+            } 
+      } 
+    }
 
 
-.chooseEmployee = function(){
-      console.log("asd"); 
-      var temp = $scope.empleados.splice(index,1);
-      $scope.working_employee.push(temp[0]);
+    $scope.chooseEmployee = function(index){
+      //console.log("asd"); 
+      //var temp = $scope.empleados.splice(index,1);
+      //console.log(temp)
+      $scope.working_employee.push(index);
       
     }//pasa empleadas que van a trabajar hoy
     
@@ -112,14 +169,19 @@ angular.module('AngularScaffold.Controllers')
     $scope.llenarEmpleado = function(){
         RoomService.GetEmpleado().then(function(response1){
           $scope.empleados = response1.data;
+          $scope.models.lists.empleados = response1.data 
+          for (var i =0 ; i <  $scope.models.lists.empleados.length; i++) {
+              $scope.models.lists.empleados.comun = $scope.models.lists.empleados.username
+           }
+          console.log(response1.data)
         });
     }
 
   	$scope.selectRoom = function(room) {
       var index = -1
       $scope.selectedRooms.indexOf(room.room_id);
-      for (var i =0; i < $scope.selectedRooms.lengths; i++) {
-        if(room.room_id == $scope.selectedRooms[i].room_id){          
+      for (i =0; i < $scope.selectedRooms.lengths; i++) {
+        if(room.room_id === $scope.selectedRooms[i].room_id){          
           index = i;
           break;          
         }
@@ -229,13 +291,20 @@ angular.module('AngularScaffold.Controllers')
     $scope.getRooms = function(){
       RoomService.GetRooms().then(function(response){
         for (var i = 0; i <response.data.length; i++) {
-          if(i < 25)
+          if(i < 25){
             $scope.firstfloor.push(response.data[i])
-          else
+            $scope.models.lists.firstfloor.comun = response.data[i].room_id
+          }
+          else{
+            $scope.models.lists.secondfloor.comun = response.data[i].room_id
             $scope.secondfloor.push(response.data[i])
-          if(response.data[i].status == 1)
+          }
+          if(response.data[i].status == 1){
             $scope.selectedRooms.push(response.data[i])
+          }
         }
+
+        $scope.sortRooms();
       })
     }
 
