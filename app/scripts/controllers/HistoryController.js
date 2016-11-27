5ff5b1b8f37d3d958fe701817dbdce2826d115f6
Elena Caballero <elena.caballero@unitec.edu>
2016-11-26 23:53:12 -0600

    Fixed problem with existing filters

--- app/scripts/controllers/HistoryController.js
+++ app/scripts/controllers/HistoryController.js
@@ -3,34 +3,27 @@ angular.module('AngularScaffold.Controllers')
   	function (HistoryService, $scope, $state, $rootScope, $sessionStorage) {
   		$scope.reportsList = [];
   		$scope.userList = [];
-  		$scope.startDate =  new Date(2015, 11, 31)
-      $scope.endDate = new Date(2019, 12, 1)
+			$scope.records = [];
+  		$scope.startDate =  new Date(2015, 11, 31);
+      $scope.endDate = new Date(2019, 12, 1);
+      $scope.recordLimit = 8;
 
   		$scope.getReports = function(){
   			HistoryService.GetReports().then(function(response){
-      		$scope.reportsList = response.data
+      		$scope.reportsList = response.data;
+      		for(var i=0; i<$scope.reportsList.length; i++) {
+					  $scope.records.push($scope.reportsList); 
+					  if ($scope.userList.indexOf($scope.reportsList[i].employee_username) === -1) {
+					   	$scope.userList.push($scope.reportsList[i].employee_username);
+					  }
+					}
   			});
 	  	};
 
-	  	$scope.mockData = [
-            {"room_number":"201", "employee_username":"dany", "room_state":"0", "problem_id":"0", "date_reported":"2016-11-20T05:00:00.000Z"},
-            {"room_number":"201", "employee_username":"dany", "room_state":"1", "problem_id":"3", "date_reported":"2016-11-08T05:00:00.000Z"},
-            {"room_number":"103", "employee_username":"dario", "room_state":"2", "problem_id":"5", "date_reported":"2016-10-26T05:00:00.000Z"},
-            {"room_number":"116", "employee_username":"katherine", "room_state":"2", "problem_id":"3", "date_reported":"2016-10-29T05:00:00.000Z"},
-            {"room_number":"114", "employee_username":"dario", "room_state":"1", "problem_id":"1", "date_reported":"2016-10-30T05:00:00.000Z"},
-            {"room_number":"114", "employee_username":"katherine", "room_state":"1", "problem_id":"2", "date_reported":"2016-11-04T05:00:00.000Z"}
-            ];
-
-      $.each($scope.reportsList, function(i, el){
-			  if($.inArray(el.employee_username, $scope.userList) === -1) {
-			  	$scope.userList.push(el.employee_username);
-			  }
-			});
-
-			$scope.filterUsernames = function(){};
+			$scope.filter = function(){};
 
 			$scope.filterByUsername = function(username){
-				return $scope.filterUsernames[username.employee_username] || $scope.noFilter($scope.filterUsernames);
+				return $scope.filter[username.employee_username] || $scope.noFilter($scope.filter);
 			};
 
 			$scope.noFilter = function(filterObj){
@@ -38,12 +31,6 @@ angular.module('AngularScaffold.Controllers')
 	      keys(filterObj).
 	      every(function (key) { return !filterObj[key]; });
 			};
-
-			$scope.records = [];
-			$scope.recordLimit = 8;
-			for(var i=0; i<$scope.reportsList.length; i++) {
-			   $scope.records.push($scope.reportsList);     
-			}
 
 			$scope.printToCart = function(printSectionId) {
         var innerContents = document.getElementById(printSectionId).innerHTML;
